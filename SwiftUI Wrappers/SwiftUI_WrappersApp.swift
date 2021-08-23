912e1ebdf7a5c3551ececc9c10ccadd02c57d90a
Sakthi Nath <sakthinath79@gmail.com>
2021-08-24 04:06:50 +0530

    Initial Commit

---
+++ SwiftUI Wrappers/SwiftUI_WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WrappersApp.swift
+//  SwiftUI Wrappers
+//
+//  Created by Admin on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
